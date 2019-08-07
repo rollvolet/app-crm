04b5658c905e46e8fb88e74524bfa6dc6aebb0a8
Erika Pauwels <erika.pauwels@gmail.com>
2019-08-07 11:34:36 +0200

    Migration to update names of fusiegemeentes

---
+++ config/migrations/20190115172100-fusiegemeenten.sql
@@ -0,0 +1,51 @@
+UPDATE [dbo].[TblPostCode]
+SET Gemeente = 'Aalter'
+WHERE Postcode = '9910'
+
+UPDATE [dbo].[TblPostCode]
+SET Gemeente = 'Deinze'
+WHERE Postcode='9850'
+
+UPDATE [dbo].[TblPostCode]
+SET Gemeente = 'Kruisem'
+WHERE Postcode='9770'
+
+UPDATE [dbo].[TblPostCode]
+SET Gemeente = 'Kruisem'
+WHERE Postcode='9750'
+
+UPDATE [dbo].[TblPostCode]
+SET Gemeente = 'Lievegem'
+WHERE Postcode='9950'
+
+UPDATE [dbo].[TblPostCode]
+SET Gemeente = 'Lievegem'
+WHERE Postcode='9920'
+
+UPDATE [dbo].[TblPostCode]
+SET Gemeente = 'Lievegem'
+WHERE Postcode='9930'
+
+UPDATE [dbo].[TblPostCode]
+SET Gemeente = 'Puurs-Sint-Amands'
+WHERE Postcode='2890'
+
+UPDATE [dbo].[TblPostCode]
+SET Gemeente = 'Puurs-Sint-Amands'
+WHERE Postcode='2870'
+
+UPDATE [dbo].[TblPostCode]
+SET Gemeente = 'Pelt'
+WHERE Postcode='3900'
+
+UPDATE [dbo].[TblPostCode]
+SET Gemeente = 'Pelt'
+WHERE Postcode='3910'
+
+UPDATE [dbo].[TblPostCode]
+SET Gemeente = 'Oudsbergen'
+WHERE Postcode='3670'
+
+UPDATE [dbo].[TblPostCode]
+SET Gemeente = 'Oudsbergen'
+WHERE Postcode='3660'
