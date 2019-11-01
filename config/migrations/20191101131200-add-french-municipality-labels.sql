e524a2a097810e7b2a4849a42f462d63ff8873a9
Erika Pauwels <erika.pauwels@gmail.com>
2019-11-01 13:16:27 +0100

    Add migration with French municipality names

---
+++ config/migrations/20191101131200-add-french-municipality-labels.sql
@@ -0,0 +1,11 @@
+INSERT INTO [dbo].[TblPostCode]
+VALUES ('7700', 'Mouscron');
+
+INSERT INTO [dbo].[TblPostCode]
+VALUES ('7850', 'Enghien');
+
+INSERT INTO [dbo].[TblPostCode]
+VALUES ('7850', 'Marcq');
+
+INSERT INTO [dbo].[TblPostCode]
+VALUES ('7850', 'Petit-Enghien');
