7049496c61c6f7508225378fc068c1c5ac277b81
Erika Pauwels <erika.pauwels@gmail.com>
2021-01-09 11:53:20 +0100

    Basic semantic.works setup

---
+++ config/authorization/config.ex
@@ -0,0 +1,36 @@
+alias Acl.Accessibility.Always, as: AlwaysAccessible
+alias Acl.GraphSpec.Constraint.Resource, as: ResourceConstraint
+alias Acl.GraphSpec, as: GraphSpec
+alias Acl.GroupSpec, as: GroupSpec
+alias Acl.GroupSpec.GraphCleanup, as: GraphCleanup
+
+defmodule Acl.UserGroups.Config do
+  def user_groups do
+    # These elements are walked from top to bottom.  Each of them may
+    # alter the quads to which the current query applies.  Quads are
+    # represented in three sections: current_source_quads,
+    # removed_source_quads, new_quads.  The quads may be calculated in
+    # many ways.  The useage of a GroupSpec and GraphCleanup are
+    # common.
+    [
+      %GroupSpec{
+        name: "rollvolet",
+        useage: [:read, :write, :read_for_write],
+        access: %AlwaysAccessible{}, # TODO check related account
+        graphs: [ %GraphSpec{
+                    graph: "http://mu.semte.ch/graphs/rollvolet",
+                    constraint: %ResourceConstraint{
+                      resource_types: [
+                      ]
+                    } } ] },
+
+      # // CLEANUP
+      #
+      %GraphCleanup{
+        originating_graph: "http://mu.semte.ch/application",
+        useage: [:write],
+        name: "clean"
+      }
+    ]
+  end
+end
