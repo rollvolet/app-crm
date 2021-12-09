8602af14ed43bfe34664286c690a83ecf49c3312
Erika Pauwels <erika.pauwels@gmail.com>
2021-12-09 15:04:30 +0100

    Add mu-cache and mu-cl-resources to the application stack

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,2 +1,16 @@
 export default [
+  {
+    match: {
+      // listen to all changes
+    },
+    callback: {
+      url: "http://resource/.mu/delta",
+      method: "POST"
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 250,
+      ignoreFromSelf: true
+    }
+  }
 ];
---
+++ config/resources/domain.lisp
@@ -0,0 +1,13 @@
+(in-package :mu-cl-resources)
+
+(defparameter *supply-cache-headers-p* t
+  "when non-nil, cache headers are supplied.  this works together with mu-cache.")
+(setf *cache-model-properties-p* t)
+(defparameter *include-count-in-paginated-responses* t
+  "when non-nil, all paginated listings will contain the number
+   of responses in the result object's meta.")
+(defparameter *cache-count-queries* t)
+(defparameter *cache-model-properties* t)
+(setf sparql:*experimental-no-application-graph-for-sudo-select-queries* t)
+
+(read-domain-file "cases.json")
