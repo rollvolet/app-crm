97c5e3db29360bea7d1c3c66e8cb6968a20710b9
Erika Pauwels <erika.pauwels@gmail.com>
2021-12-26 19:22:43 +0100

    Update authorization config
    - add public group with read-only data
    - require user to be logged in to access data from rollvolet-group

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -1,22 +1,52 @@
 alias Acl.Accessibility.Always, as: AlwaysAccessible
+alias Acl.Accessibility.ByQuery, as: AccessByQuery
 alias Acl.GraphSpec.Constraint.Resource, as: ResourceConstraint
+alias Acl.GraphSpec.Constraint.ResourceFormat, as: ResourceFormatConstraint
 alias Acl.GraphSpec, as: GraphSpec
 alias Acl.GroupSpec, as: GroupSpec
 alias Acl.GroupSpec.GraphCleanup, as: GraphCleanup
 
 defmodule Acl.UserGroups.Config do
+
+  defp authenticated_access() do
+    %AccessByQuery{
+      vars: [],
+      query: "PREFIX session: <http://mu.semte.ch/vocabularies/session/>
+      PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+      PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+      SELECT ?account WHERE {
+          <SESSION_ID> session:account ?account .
+      } LIMIT 1"
+    }
+  end
+
   def user_groups do
-    # These elements are walked from top to bottom.  Each of them may
-    # alter the quads to which the current query applies.  Quads are
-    # represented in three sections: current_source_quads,
-    # removed_source_quads, new_quads.  The quads may be calculated in
-    # many ways.  The useage of a GroupSpec and GraphCleanup are
-    # common.
     [
+      %GroupSpec{
+        name: "public",
+        useage: [:read],
+        access: %AlwaysAccessible{},
+        graphs: [
+          %GraphSpec{
+            graph: "http://mu.semte.ch/graphs/public",
+            constraint: %ResourceConstraint{
+              resource_types: [
+              ]
+            }
+          },
+          %GraphSpec{
+            graph: "http://mu.semte.ch/graphs/sessions",
+            constraint: %ResourceFormatConstraint{
+              resource_prefix: "http://mu.semte.ch/sessions/"
+            }
+          }
+        ]
+      },
+
       %GroupSpec{
         name: "rollvolet",
         useage: [:read, :write, :read_for_write],
-        access: %AlwaysAccessible{}, # TODO check related account
+        access: authenticated_access(),
         graphs: [ %GraphSpec{
                     graph: "http://mu.semte.ch/graphs/rollvolet",
                     constraint: %ResourceConstraint{
