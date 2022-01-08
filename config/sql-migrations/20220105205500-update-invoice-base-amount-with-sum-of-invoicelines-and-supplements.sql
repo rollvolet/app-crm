ce88a6f36cab48056a0dcf3a67b85f73156cae8c
Erika Pauwels <erika.pauwels@gmail.com>
2022-01-08 17:27:56 +0100

    Add migrations to recalculate base amount of invoice
    Base amount is the sum of invoicelines and supplements

--- config/sql-migrations/20220105205500-update-invoice-base-amount-with-sum-of-invoicelines-and-supplements.sql
+++ config/sql-migrations/20220105205500-update-invoice-base-amount-with-sum-of-invoicelines-and-supplements.sql
@@ -2,9 +2,9 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 -- Update the base amount of all invoices, that are not deposit-invoices,
--- to the sum of its invoicelines
+-- to the sum of its invoicelines and supplements
 UPDATE f
-SET f.BasisBedrag = COALESCE(Invoicelines.BestelTotaalNetto, 0)
+SET f.BasisBedrag = COALESCE(Invoicelines.BestelTotaalNetto, 0) + COALESCE(Supplements.Totaal, 0)
 FROM TblFactuur f
 LEFT JOIN TblVoorschotFactuur hub ON hub.VoorschotFactuurID = f.FactuurId
 LEFT JOIN (
@@ -12,6 +12,11 @@ LEFT JOIN (
   FROM TblInvoiceline l
   GROUP BY l.InvoiceId
 ) as Invoicelines ON Invoicelines.InvoiceId = f.FactuurId
+LEFT JOIN (
+  SELECT s.FactuurID as InvoiceId, SUM(s.NettoBedrag) as Totaal
+  FROM TblFactuurExtra s
+  GROUP BY s.FactuurID
+) as Supplements ON Supplements.InvoiceId = f.FactuurId
 WHERE f.MuntEenheid = 'EUR' AND hub.VoorschotId IS NULL
 GO
 
