cf3482ba2a08225aaca3f3a4c1fa4689d2f3889a
Erika Pauwels <erika.pauwels@gmail.com>
2022-05-04 14:56:55 +0200

    Add migration for family as honorific prefix

---
+++ config/sql-migrations/20220504144900-add-honorific-prefix-family.sql
@@ -0,0 +1,7 @@
+INSERT INTO klanten.dbo.TblAanspreektitel(AanspreekID, TaalId, Omschrijving)
+VALUES (16, 1, 'Familie')
+GO
+
+INSERT INTO klanten.dbo.TblAanspreektitel(AanspreekID, TaalId, Omschrijving)
+VALUES (16, 2, 'Famille')
+GO
